c97deb5ef5623e8400f6ef008bede17559982672
TAHA MEHDI <mehdit@rpi.edu>
2013-11-05 19:13:07 -0500

    placeholders for header and slider

---
+++ index.php
@@ -0,0 +1,11 @@
+	<?php include 'header.php'; ?>
+	<div id = "content">
+		<div id ="intro">
+			Confiden is a place for young adults to come to build their confidence levels, no matter their current perceived level of confidence. They must complete challenges, A.K.A. "missions" with the intention of building a solid foundation of healthy confidence. Confiden has the potential to change the lives of any people willing to put the work towards changing their lives for the better, ideally gradually leaving their comfort zones and becoming more independent, self-assured individuals. Etc etc we the best
+		</div>
+		<div id = "slider">
+				Super dope slider right here
+				<!-- slider will go here -->
+		</div>
+	</div>
+	
