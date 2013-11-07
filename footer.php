378860e306e1d39d8767fdaf144c7d4e30ec71f8
Hydriad <Hydriad@users.noreply.github.com>
2013-11-07 15:02:40 -0500

    working footer links

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
 		<div id ="footer">
 			<p id = "footerlinks">
-				<a href="#">About</a>
-				<a href="#">Contact</a>
-				<a href="#">Resources</a>
-				<a href="#">Privacy Policy</a>
-				<a href="#">Terms and Conditions</a>
-				<a href="#">Blah</a>
+				<a href="faq.php">FAQ</a>
+				<a href="contact.php">Contact Us</a>
+				<a href="resources.php">Resources</a>
+				<a href="privacypolicy.php">Privacy Policy</a>
+				<a href="termsandconditions.php">Terms and Conditions</a>
 			</p>
 		</div>
 	</body>
