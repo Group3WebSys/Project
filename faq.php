8164383b9294c3f6ade31b5bfd8b198830b4aef2
Hydriad <Hydriad@users.noreply.github.com>
2013-11-07 14:56:27 -0500

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+	<div class = "bodyBlock">
+			<h1>FAQ</h1>
+			<p>Yo, we are awesome.</p>
+	</div>
+	<?php include 'leftsidebar.php'; ?>
+	<?php include 'rightsidebar.php'; ?>
+<?php include 'footer.php'; ?>
+
