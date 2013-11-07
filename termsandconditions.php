a2a966030be6aadaa8313437b5f8d113e5ab23db
Hydriad <Hydriad@users.noreply.github.com>
2013-11-07 14:54:53 -0500

    Create termsandconditions.php

---
+++ termsandconditions.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+	<div class = "bodyBlock">
+			<h1>TERMS AND CONDITIONS</h1>
+			<p>Yo, we are awesome.</p>
+	</div>
+	<?php include 'leftsidebar.php'; ?>
+	<?php include 'rightsidebar.php'; ?>
+<?php include 'footer.php'; ?>
+
