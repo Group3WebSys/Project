cbef81bab9a013709d20aaf86f97101426caf03b
Hydriad <Hydriad@users.noreply.github.com>
2013-11-07 14:35:52 -0500

    Update missions.php

--- missions.php
+++ missions.php
@@ -1,16 +1,17 @@
 <?php include 'header.php'; ?>
-		<div id = "bodyBlock">
+	<div id = "bodyBlock">
 			<h1>MISSIONS</h1>
 			<ul id = "missionsList">
-				<li>Be real</li>
-				<li>Be freaky</li>
-				<li>Be awesome</li>
-				<li>Be ballin</li>
-				<li>Be Taha</li>
-			</ul>
-		</div>
-		<!--left sidebar-->
-		<?php include 'leftsidebar.php'; ?>
-		<?php include 'rightsidebar.php'; ?>
+			<br/>
+			<p>()This list will have to be replaced with a jquery menu)</p>
+			<li>Be real</li>
+			<li>Be freaky</li>
+			<li>Be awesome</li>
+			<li>Be ballin</li>
+			<li>Be Taha</li>
+		</ul>
+	</div>
+	<?php include 'leftsidebar.php'; ?>
+	<?php include 'rightsidebar.php'; ?>
 <?php include 'footer.php'; ?>
 
