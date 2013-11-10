eb71a06599e786f0a286675fdc461375b853bd5f
Hydriad <Hydriad@users.noreply.github.com>
2013-11-10 17:42:26 -0500

    Update faq.php

--- faq.php
+++ faq.php
@@ -1,7 +1,18 @@
 <?php include 'header.php'; ?>
 	<div class = "bodyBlock">
-			<h1>FAQ</h1>
-			<p>Yo, we are awesome.</p>
+		<h1>FAQ</h1>
+		<h2>How many levels are there?</h2>
+		<p>You can reach up to level 10 as of now, but we are working towards making more in the future.</p>
+		<h2>I’m not comfortable completing some missions. What do I do?</h2>
+		<p>We aim to guide our users out of their comfort zones, but at the same time do not want to push anyone too far. There is some leeway allowed in gaining levels without completing a few missions each time, but they will remain as options to come back to later.  If you truly feel you’re not ready to complete any of the missions, check out our <a href = "resources.php">Resources Page</a> to see if any of the articles, books, or other websites listed can help you.</p>
+		<h2>Can’t I just cheat and say I’ve completed a mission?</h2>
+		<p>Yes, you can. However, the only person you’d be cheating by doing that is yourself. Confiden’s mission is to help guide anyone who is willing to put the work towards changing their lives for the better by providing the tools to build a solid foundation of healthy self-confidence.</p>
+		<h2>I’ve completed all 10 levels! What now?</h2>
+		<p>Confiden is merely a temporary guide in your journey of self-confidence; we can’t keep giving missions forever! After completing our missions, you’ve had a taste of what it’s like to leave your comfort zone. However, you know more about yourself than we do, so now it is up to you to take the next steps by facing more personal challenges. Good luck!</p>			
+		<h2>Does Confiden count as therapy or treatment for any psychological disorders?</h2>
+		<p>No. Trying to become confident through Confiden does not take the place of attending sessions with therapists or psychiatrists, nor does it take the place of perscription medication. Feel free to talk about your progress in Confiden with your friends, family, or trusted mental health professionals, but do not use it as a replacement for any perscribed appointments or medications.</p>
+		<h2>Confiden is not working for me. What do I do?</h2>
+		<p>Confiden isn't for everyone; we totally uderstand that! If you feel that Confiden isn't your jam, check out our <a href = "resources.php">Resources Page</a> to see if any of the articles, books, or other websites listed can help you.</p>
 	</div>
 	<?php include 'leftsidebar.php'; ?>
 	<?php include 'rightsidebar.php'; ?>
