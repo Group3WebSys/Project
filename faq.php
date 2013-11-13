d3ee6cbe3dfc7268230bb9c21a2b6d435e479c23
Hydriad <Hydriad@users.noreply.github.com>
2013-11-13 11:23:43 -0500

    Update faq.php

--- faq.php
+++ faq.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 	<div class = "bodyBlock">
 		<h1>FAQ</h1>
-		
+		<br/>
 		<script>  
                 function toggle_visibility(id) {
                   $("#" + id).toggle();
@@ -9,46 +9,46 @@
 		</script>
 
 		<div id="q1teaser" style="display: inline;">
-			<p><a href="#" onclick="toggle_visibility('q1details');">How many levels are there?</a></p>
+			<p><a href="#" onclick="toggle_visibility('q1details');"><h3>How many levels are there?</h3></a></p>
 		</div>
 		<div id = "q1details" class = "hiddendiv" style = "display: none;">
 			<p>You can reach up to level 10 as of now, but we are working towards making more in the future.</p>
 		</div>
 		
 		<div id="q2teaser" style="display: inline;">
-			<p><a href="#" onclick="toggle_visibility('q2details');">I'm not comfortable completing some missions. What do I do?</a></p>
+			<p><a href="#" onclick="toggle_visibility('q2details');"><h3>I'm not comfortable completing some missions. What do I do?</h3></a></p>
 		</div>
 		<div id = "q2details" class = "hiddendiv" style = "display: none;">
 			<p>We aim to guide our users out of their comfort zones, but at the same time do not want to push anyone too far. There is some leeway allowed in gaining levels without completing a few missions each time, but they will remain as options to come back to later. If you truly feel you're not ready to complete any of the missions, check out our <a href = "resources.php">Resources Page</a> to see if any of the articles, books, or other websites listed can help you.</p>
 		</div>
 		
 		<div id="q3teaser" style="display: inline;">
-			<p><a href="#" onclick="toggle_visibility('q3details');">Can't I just cheat and say I've completed a mission?</a></p>
+			<p><a href="#" onclick="toggle_visibility('q3details');"><h3>Can't I just cheat and say I've completed a mission?</h3></a></p>
 		</div>
 		<div id = "q3details" class = "hiddendiv" style = "display: none;">
 			<p>Yes, you can. However, the only person you'd be cheating by doing that is yourself. Confiden's mission is to help guide anyone who is willing to put the work towards changing their lives for the better by providing the tools to build a solid foundation of healthy self-confidence.</p>
 		</div>
 		
 		<div id="q4teaser" style="display: inline;">
-			<p><a href="#" onclick="toggle_visibility('q4details');">I've completed all 10 levels! What now?</a></p>
+			<p><a href="#" onclick="toggle_visibility('q4details');"><h3>I've completed all 10 levels! What now?</h3></a></p>
 		</div>
 		<div id = "q4details" class = "hiddendiv" style = "display: none;">
 			<p>Confiden is merely a temporary guide in your journey of self-confidence; we can't keep giving missions forever! After completing our missions, you've had a taste of what it's like to leave your comfort zone. However, you know more about yourself than we do, so now it is up to you to take the next steps by facing more personal challenges. Good luck!</p>			
 		</div>		
 		
 		<div id="q5teaser" style="display: inline;">
-			<p><a href="#" onclick="toggle_visibility('q5details');">Does Confiden count as therapy or treatment for my psychological disorder?</a></p>
+			<p><a href="#" onclick="toggle_visibility('q5details');"><h3>Does Confiden count as therapy or treatment for my psychological disorder?</h3></a></p>
 		</div>
 		<div id = "q5details" class = "hiddendiv" style = "display: none;">
 			<p>No. Trying to become confident through Confiden does not take the place of attending sessions with therapists or psychiatrists, nor does it take the place of perscription medication. Feel free to talk about your progress in Confiden with your friends, family, or trusted mental health professionals, but do not use it as a replacement for any perscribed appointments or medications.</p>
 		</div>		
 		
 		<div id="q6teaser" style="display: inline;">
-			<p><a href="#" onclick="toggle_visibility('q6details');">Confiden is not working for me. What do I do?</a></p>
+			<p><a href="#" onclick="toggle_visibility('q6details');"><h3>Confiden is not working for me. What do I do?</h3></a></p>
 		</div>
 		<div id = "q6details" class = "hiddendiv" style = "display: none;">
 			<p>Confiden isn't for everyone; we totally uderstand that! If you feel that Confiden isn't your jam, check out our <a href = "resources.php">Resources Page</a> to see if any of the articles, books, or other websites listed can help you.</p>
-		</div>		
+		</div>	
 		
 	</div>
 	<?php include 'leftsidebar.php'; ?>
