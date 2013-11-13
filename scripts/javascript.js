e573ca7a1015aa40077c467b7b84c886024e0a5b
Hydriad <Hydriad@users.noreply.github.com>
2013-11-13 11:01:18 -0500

    Update javascript.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,7 +1,3 @@
-function showq( q ) {
-  document.getElementById( q ).style.display = 'inline';
-}
-
-function hideq( q ) {
-  document.getElementById( q ).style.display = 'none';
-}
+		function toggle_visibility(id) {
+  		$("#" + id).toggle();
+		} 
