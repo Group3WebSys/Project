4ad5392594246ccd133b1cfb61240b33e371a054
Hydriad <Hydriad@users.noreply.github.com>
2013-11-13 10:59:28 -0500

    Update faq.php

--- faq.php
+++ faq.php
@@ -16,26 +16,40 @@
 		</div>
 		
 		<div id="q2teaser" style="display: inline;">
-			<p><a href="#" onclick="toggle_visibility('q1details');"><h2>I'm not comfortable completing some missions. What do I do?</h2></a></p>
+			<p><a href="#" onclick="toggle_visibility('q2details');">I'm not comfortable completing some missions. What do I do?</a></p>
 		</div>
 		<div id = "q2details" class = "hiddendiv" style = "display: none;">
 			<p>We aim to guide our users out of their comfort zones, but at the same time do not want to push anyone too far. There is some leeway allowed in gaining levels without completing a few missions each time, but they will remain as options to come back to later. If you truly feel you're not ready to complete any of the missions, check out our <a href = "resources.php">Resources Page</a> to see if any of the articles, books, or other websites listed can help you.</p>
 		</div>
 		
-		<h2>Can't I just cheat and say I've completed a mission?</h2>
-		<p>Yes, you can. However, the only person you'd be cheating by doing that is yourself. Confiden's mission is to help guide anyone who is willing to put the work towards changing their lives for the better by providing the tools to build a solid foundation of healthy self-confidence.</p>
+		<div id="q3teaser" style="display: inline;">
+			<p><a href="#" onclick="toggle_visibility('q3details');">Can't I just cheat and say I've completed a mission?</a></p>
+		</div>
+		<div id = "q3details" class = "hiddendiv" style = "display: none;">
+			<p>Yes, you can. However, the only person you'd be cheating by doing that is yourself. Confiden's mission is to help guide anyone who is willing to put the work towards changing their lives for the better by providing the tools to build a solid foundation of healthy self-confidence.</p>
+		</div>
 		
+		<div id="q4teaser" style="display: inline;">
+			<p><a href="#" onclick="toggle_visibility('q4details');">I've completed all 10 levels! What now?</a></p>
+		</div>
+		<div id = "q4details" class = "hiddendiv" style = "display: none;">
+			<p>Confiden is merely a temporary guide in your journey of self-confidence; we can't keep giving missions forever! After completing our missions, you've had a taste of what it's like to leave your comfort zone. However, you know more about yourself than we do, so now it is up to you to take the next steps by facing more personal challenges. Good luck!</p>			
+		</div>		
 		
-		<h2>I've completed all 10 levels! What now?</h2>
-		<p>Confiden is merely a temporary guide in your journey of self-confidence; we can't keep giving missions forever! After completing our missions, you’ve had a taste of what it's like to leave your comfort zone. However, you know more about yourself than we do, so now it is up to you to take the next steps by facing more personal challenges. Good luck!</p>			
+		<div id="q5teaser" style="display: inline;">
+			<p><a href="#" onclick="toggle_visibility('q5details');">Does Confiden count as therapy or treatment for my psychological disorder?</a></p>
+		</div>
+		<div id = "q5details" class = "hiddendiv" style = "display: none;">
+			<p>No. Trying to become confident through Confiden does not take the place of attending sessions with therapists or psychiatrists, nor does it take the place of perscription medication. Feel free to talk about your progress in Confiden with your friends, family, or trusted mental health professionals, but do not use it as a replacement for any perscribed appointments or medications.</p>
+		</div>		
 		
+		<div id="q6teaser" style="display: inline;">
+			<p><a href="#" onclick="toggle_visibility('q6details');">Confiden is not working for me. What do I do?</a></p>
+		</div>
+		<div id = "q6details" class = "hiddendiv" style = "display: none;">
+			<p>Confiden isn't for everyone; we totally uderstand that! If you feel that Confiden isn't your jam, check out our <a href = "resources.php">Resources Page</a> to see if any of the articles, books, or other websites listed can help you.</p>
+		</div>		
 		
-		<h2>Does Confiden count as therapy or treatment for any psychological disorders?</h2>
-		<p>No. Trying to become confident through Confiden does not take the place of attending sessions with therapists or psychiatrists, nor does it take the place of perscription medication. Feel free to talk about your progress in Confiden with your friends, family, or trusted mental health professionals, but do not use it as a replacement for any perscribed appointments or medications.</p>
-		
-		
-		<h2>Confiden is not working for me. What do I do?</h2>
-		<p>Confiden isn't for everyone; we totally uderstand that! If you feel that Confiden isn't your jam, check out our <a href = "resources.php">Resources Page</a> to see if any of the articles, books, or other websites listed can help you.</p>
 	</div>
 	<?php include 'leftsidebar.php'; ?>
 	<?php include 'rightsidebar.php'; ?>
