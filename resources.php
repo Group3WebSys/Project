7e1bfebabb14fc1abd79c28187a5bbbeca556718
Taylor Davis <polkadotsandtea@gmail.com>
2013-11-21 16:30:23 -0500

    adding some resources

--- resources.php
+++ resources.php
@@ -1,7 +1,11 @@
 <?php include 'header.php'; ?>
 	<div class = "bodyBlock">
 			<h1>RESOURCES</h1>
-			<p>Yo, we are awesome.</p>
+			<p><a href="http://www.suicidepreventionlifeline.org/">NATIONAL SUICIDE PREVENTION LIFELINE</a> 1-800-273-TALK</p>
+			<p><a href="http://www.huffingtonpost.co.uk/paul-bailey/building-confidence-top-tips_b_3032604.html">Twenty-Three Top Tips to Building Confidence</a></p>
+			<p><a href="http://www.webmd.com/anxiety-panic/guide/coping-with-anxiety">Coping with Anxiety</a></p>
+			<p><a href="http://www.psychologytoday.com/blog/hide-and-seek/201205/building-confidence-and-self-esteem">Building Confidence and Self-Esteem</a></p>
+			<p><a href="http://healthland.time.com/2012/10/08/6-breathing-exercises-to-relax-in-10-minutes-or-less/">6 Breathing Exercises to Relax in Ten Minutes or Less</a></p>
 	</div>
 	<?php include 'leftsidebar.php'; ?>
 	<?php include 'rightsidebar.php'; ?>
