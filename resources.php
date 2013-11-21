b74541de5c09cf5c5046a83551e768116f51109c
Taylor Davis <polkadotsandtea@gmail.com>
2013-11-21 17:50:16 -0500

    Update resources.php

--- resources.php
+++ resources.php
@@ -1,8 +1,8 @@
 <?php include 'header.php'; ?>
 	<div class = "bodyBlock">
 		<h1>RESOURCES</h1>
+		<h2><a href="http://www.suicidepreventionlifeline.org/">NATIONAL SUICIDE PREVENTION LIFELINE</a> 1-800-273-TALK</h2>
 		<h3>Articles</h3>
-			<p><a href="http://www.suicidepreventionlifeline.org/">NATIONAL SUICIDE PREVENTION LIFELINE</a> 1-800-273-TALK</p>
 			<p><a href="http://www.huffingtonpost.co.uk/paul-bailey/building-confidence-top-tips_b_3032604.html">Twenty-Three Top Tips to Building Confidence</a></p>
 			<p><a href="http://www.webmd.com/anxiety-panic/guide/coping-with-anxiety">Coping with Anxiety</a></p>
 			<p><a href="http://www.psychologytoday.com/blog/hide-and-seek/201205/building-confidence-and-self-esteem">Building Confidence and Self-Esteem</a></p>
