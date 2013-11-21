5032ae8b284c63eb3c4ec1368640e20a77012fcb
nurgul92 <nurgul.kantarbayeva@gmail.com>
2013-11-21 16:56:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,80 @@
 			Confiden is a place for young adults to come to build their confidence levels, no matter their current perceived level of confidence. They must complete challenges, A.K.A. "missions" with the intention of building a solid foundation of healthy confidence. Confiden has the potential to change the lives of any people willing to put the work towards changing their lives for the better, ideally gradually leaving their comfort zones and becoming more independent, self-assured individuals. Etc etc we the best
 		</div>
 		<div id = "slider">
-				Super dope slider right here
-				<!-- slider will go here -->
+												
+		<div class="slider-wrap">
+			<div id="main-photo-slider" class="csw">
+				<div class="panelContainer">
+					<div class="panel" title="Panel 1">
+						<div class="wrapper">
+							<img src="style/resources/tempphoto-1.jpg" alt="temp" />
+							<div class="photo-meta-data">
+								<a href="#">Optimistic link</a>
+							</div>
+						</div>
+					</div>
+					<div class="panel" title="Panel 2">
+						<div class="wrapper">
+							<img src="style/resources/tempphoto-2.jpg" alt="temp" />
+							<div class="photo-meta-data">
+								<a href="#">Link</a>
+							</div>
+						</div>
+					</div>		
+					<div class="panel" title="Panel 3">
+						<div class="wrapper">
+							<img src="style/resources/tempphoto-3.jpg" alt="temp"/>
+							<div class="photo-meta-data">
+								<a href="#">Link</a>
+							</div>
+						</div>
+					</div>
+					<div class="panel" title="Panel 4">
+						<div class="wrapper">
+							<img src="style/resources/tempphoto-4.jpg" alt="temp" />
+							<div class="photo-meta-data">
+								<a href="#">Link</a>
+							</div>
+						</div>
+					</div>
+					<div class="panel" title="Panel 5">
+						<div class="wrapper">
+							<img src="style/resources/tempphoto-5.jpg" alt="temp" />
+							<div class="photo-meta-data">
+								<a href="#">Link</a>
+							</div>
+						</div>
+					</div>
+					<div class="panel" title="Panel 6">
+						<div class="wrapper">
+							<h1>A Tale of Two Cities</h1>
+							<p><em>Charles Dickins</em></p>
+							<blockquote>It was the best of times, it was the worst of times, it was the age of wisdom,
+							 it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity,
+							 it was the season of Light, it was the season of Darkness, it was the spring of hope,
+							 it was the winter of despair, we had everything before us, we had nothing before us, 
+							 we were all going direct to heaven, we were all going direct the other way - in short,
+							 the period was so far like the present period, that some of its noisiest authorities insisted on its being received,
+							 for good or for evil, in the superlative degree of comparison only.
+							</blockquote>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<a href="#1" class="cross-link active-thumb"><img src="style/resources/tempphoto-1thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a>
+			<div id="movers-row">
+				<div><a href="#2" class="cross-link"><img src="style/resources/tempphoto-2thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a></div>
+				<div><a href="#3" class="cross-link"><img src="style/resources/tempphoto-3thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a></div>
+				<div><a href="#4" class="cross-link"><img src="style/resources/tempphoto-4thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a></div>
+				<div><a href="#5" class="cross-link"><img src="style/resources/tempphoto-5thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a></div>
+				<div><a href="#6" class="cross-link"><img src="style/resources/tempphoto-6thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a></div>
+			</div>
+
 		</div>
+	
+	</div>
 		<div id = "widgets">
 			<div class="widget">Widget 1</div>
 			<div class="widget">Widget 2</div>
