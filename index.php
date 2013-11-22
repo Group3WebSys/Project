e985fd765eec58955812e383737ed2492fe997be
TAHA MEHDI <mehdit@rpi.edu>
2013-11-22 16:24:46 -0500

    sidebar on index? (does it look good?)

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 	<?php include 'header.php'; ?>
+	<?php include 'rightsidebar.php'; ?>
 	<div id = "content">
 		<div id ="intro">
 			Confiden is a place for young adults to come to build their confidence levels, no matter their current perceived level of confidence. They must complete challenges, A.K.A. "missions" with the intention of building a solid foundation of healthy confidence. Confiden has the potential to change the lives of any people willing to put the work towards changing their lives for the better, ideally gradually leaving their comfort zones and becoming more independent, self-assured individuals. Etc etc we the best
