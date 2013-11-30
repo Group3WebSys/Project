d42969d6048ecf590603eab630779a22122d437a
Taylor Davis <polkadotsandtea@gmail.com>
2013-11-30 03:55:38 -0500

    form added

--- journal.php
+++ journal.php
@@ -1,7 +1,12 @@
 <?php include 'header.php'; ?>
 	<div class = "bodyBlock">
 			<h1>JOURNAL</h1>
-			<p>Yo, we are awesome.</p>
+			<!--STILL NEEDS CODE TO ADD INPUT DATA TO TABLE-->
+			<form action=""  method="post" id="journal">
+				<label>Subject: </label><input type="text" name="subject" /><br>
+				<input type="text" name="entry" style="width:750px; height:400px;"/><br>
+				<input id="button_journal" type="submit" value="Post"/>
+			</form>
 	</div>
 	<?php include 'leftsidebar.php'; ?>
 	<?php include 'rightsidebar.php'; ?>
