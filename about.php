65540ea49b830b775605f12d29a38728acac9a58
Hydriad <Hydriad@users.noreply.github.com>
2013-12-02 14:20:06 -0500

    polishing the about

--- about.php
+++ about.php
@@ -1,13 +1,12 @@
 <?php include 'header.php'; ?>
 	<div class = "bodyBlock">
-		<h1>ABOUT US</h1>
-		<p>Yo, we are awesome.</p>
+		<h1>ABOUT US</h1><br/>
 		<h3>Overview</h3>
-		<p>Confiden is a place for people to use to build their confidence levels, no matter their current perceived level of confidence. Users must complete challenges, A.K.A. "Missions" with the intention of building a solid foundation of healthy confidence. Confiden has the potential to assist anyone willing to put the work towards changing their lives for the better, gradually leaving their comfort zones and becoming more independent, self-assured individuals.
+		<p>Confiden is a place for people to use to build their confidence levels, no matter their current perceived level of confidence. Users must complete challenges, A.K.A. "Missions" with the intention of building a solid foundation of healthy confidence. Confiden has the potential to assist anyone willing to put the work towards changing their lives for the better, gradually leaving their comfort zones and becoming more independent, self-assured individuals.</p>
 		<h3>Our Mission</h3>
-		</p><p>
+		</p>To help guide individuals who feel that they lack confidence and are looking to build theirs by providing the resources and opportunities to do so in a safe and fun way.<p><br/>
 		<h3>Free for All</h3>
-		</p>Confiden is a 100% free website for all to use. We will not charge our users, who are here to seek help, for our assistance.<p>
+		</p>Confiden is a 100% free website for all to use. We will not charge our users, who are here to seek help, for the assistance we provide.<p><br/>
 		<h3>Who We Are</h3>
 		<p>Alison Bischak!</p><img src = "style/resources/about_alison.jpg" alt = "whoops"><br/><br/>
 		<p>Taylor Davis!</p><img src = "style/resources/about_taylor.jpg" alt = "whoops"><br/><br/>
