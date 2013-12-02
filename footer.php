acb79d863b85d477c7f06914d14bf956fe917072
Hydriad <Hydriad@users.noreply.github.com>
2013-12-02 14:06:25 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,10 @@
 		<div id = "push"></div>
 		<div id ="footer">
-			<span id = "footerlinks" style = 'padding-right:5px; display: inline-block;' >
-				<a href="faq.php">FAQ</a>
-				<a href="contact.php">Contact Us</a>
-				<a href="resources.php">Resources</a>
-				<a href="privacypolicy.php">Privacy Policy</a>
-				<a href="termsandconditions.php">Terms and Conditions</a>
-			</span>
+				<span style = 'padding-right:5px;' ><a href="faq.php">FAQ</a></span>
+				<span style = 'padding-right:5px;' ><a href="contact.php">Contact Us</a></span>
+				<span style = 'padding-right:5px;' ><a href="resources.php">Resources</a></span>
+				<span style = 'padding-right:5px;' ><a href="privacypolicy.php">Privacy Policy</a></span>
+				<span style = 'padding-right:5px;' ><a href="termsandconditions.php">Terms and Conditions</a></span>
 		</div>
 	</body>
 </html>
