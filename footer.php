72a94859e53c79169257509d63653ca963388936
Hydriad <Hydriad@users.noreply.github.com>
2013-12-02 13:58:40 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
 		<div id = "push"></div>
 		<div id ="footer">
-			<p id = "footerlinks">
-				<a href="faq.php">FAQ</a>
-				<a href="contact.php">Contact Us</a>
-				<a href="resources.php">Resources</a>
-				<a href="privacypolicy.php">Privacy Policy</a>
-				<a href="termsandconditions.php">Terms and Conditions</a>
-			</p>
+			<span id = "footerlinks">
+				<ul id = "footerlinksul">
+				<li><a href="faq.php">FAQ</a></li>
+				<li><a href="contact.php">Contact Us</a></li>
+				<li><a href="resources.php">Resources</a></li>
+				<li><a href="privacypolicy.php">Privacy Policy</a></li>
+				<li><a href="termsandconditions.php">Terms and Conditions</a></li>
+			</span>
 		</div>
 	</body>
 </html>
