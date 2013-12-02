ef7e3eb0b8387245985372d5c6ed76d4beb8e128
Hydriad <Hydriad@users.noreply.github.com>
2013-12-02 13:10:28 -0500

    ballin' about us page, yo

--- about.php
+++ about.php
@@ -1,7 +1,22 @@
 <?php include 'header.php'; ?>
 	<div class = "bodyBlock">
-			<h1>ABOUT</h1>
-			<p>Yo, we are awesome.</p>
+		<h1>ABOUT US</h1>
+		<p>Yo, we are awesome.</p>
+		<h3>Overview</h3>
+		<p>Confiden is a place for people to use to build their confidence levels, no matter their current perceived level of confidence. Users must complete challenges, A.K.A. "Missions" with the intention of building a solid foundation of healthy confidence. Confiden has the potential to assist anyone willing to put the work towards changing their lives for the better, gradually leaving their comfort zones and becoming more independent, self-assured individuals.
+		<h3>Our Mission</h3>
+		</p><p>
+		<h3>Free for All</h3>
+		</p><p>
+		<h3>Who We Are</h3>
+		<img src = "about_.jpg">
+		<img src = "about_.jpg">
+		</p>
+		<p>
+
+		
+		</p>
+		<p>Feel free to refer to our <a href = "faq.php">Resources Page</a> or <a href = "contact.php">Contact Us</a> if you have any further questions.</p>
 	</div>
 	<?php include 'leftsidebar.php'; ?>
 	<?php include 'rightsidebar.php'; ?>
