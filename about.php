f7133063cdecc98d7704606cbc92efb3eb22749d
Hydriad <Hydriad@users.noreply.github.com>
2013-12-02 13:19:51 -0500

    about us, yo

--- about.php
+++ about.php
@@ -7,15 +7,14 @@
 		<h3>Our Mission</h3>
 		</p><p>
 		<h3>Free for All</h3>
-		</p><p>
+		</p>Confiden is a 100% free website for all to use. We will not charge our users, who are here to seek help, for our assistance.<p>
 		<h3>Who We Are</h3>
-		<img src = "about_.jpg">
-		<img src = "about_.jpg">
-		</p>
-		<p>
-
-		
-		</p>
+		<p>Alison Bischak!</p><img src = "about_alison.jpg" alt = "whoops"><br/>
+		<p>Taylor Davis!</p><img src = "about_taylor.jpg" alt = "whoops"><br/>
+		<p>Nurgul Kantarbayeva!</p><img src = "about_nurgul.jpg" alt = "whoops"><br/>
+		<p>Taha Mehdi!</p><img src = "about_taha.jpg" alt = "whoops"><br/>
+		<p>Hayley Schluter!</p><img src = "about_hayley.jpg" alt = "whoops"><br/>
+		<p>Michael Xie!</p><img src = "about_michael.jpg" alt = "whoops"><br/>
 		<p>Feel free to refer to our <a href = "faq.php">Resources Page</a> or <a href = "contact.php">Contact Us</a> if you have any further questions.</p>
 	</div>
 	<?php include 'leftsidebar.php'; ?>
