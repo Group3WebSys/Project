4082be2c3f69e23d44649de3c98a25af7ba6bd15
Hydriad <Hydriad@users.noreply.github.com>
2013-12-03 09:01:14 -0500

    having trouble with journal -.-

--- journal.php
+++ journal.php
@@ -1,37 +1,7 @@
-<?php include 'header.php';
-session_start();
-  // Connect to the database
-  try {
-    $dbname = 'test1';
-    $user = 'root';
-    $pass = '';
-    $dbconn = new PDO('mysql:host=localhost;dbname='.$dbname, $user, $pass);
-  }
-  catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-  }
-  
-//entering the journal
-if (isset($_SESSION['username']) ) :
-
-	if (!isset($_POST['subject']) || !isset($_POST['entry']) || empty($_POST['subject']) || empty($_POST['entry']) ) {
-	      $msg = "Please fill in all form fields.";
-	    }
-
-	else{
-	 $stmt = $dbconn->prepare("INSERT INTO journals (uid, author, content, date) VALUES (:uid, :author, :content, :date)");
-	      $stmt->execute(array(':uid' => $_POST['name'], ':author' => $_SESSION['username'], ':content' => $_POST['entry'], ':date' => date("Y/m/d") ));
-	      $msg = "Journal entry recorded.";
-	}
-	
-else: echo "ya done goofed"; 
-?>
+<?php include 'header.php';?>
 
 
-
-
-
-	<div class = "bodyBlock">
+ 	<div class = "bodyBlock">
 			<h1>JOURNAL</h1>
 			<!--STILL NEEDS CODE TO ADD INPUT DATA TO TABLE-->
 			<form action=""  method="post" id="journal">
