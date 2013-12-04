d0c1f87407da6abdbf6132c26708f3ec267900de
Hydriad <Hydriad@users.noreply.github.com>
2013-12-03 22:18:07 -0500

    Update resources.php

--- resources.php
+++ resources.php
@@ -1,7 +1,8 @@
 <?php include 'header.php'; ?>
+<div class = "wrapmeup">
 	<div class = "bodyBlock">
 		<h1>RESOURCES</h1>
-		<h2><a href="http://www.suicidepreventionlifeline.org/">NATIONAL SUICIDE PREVENTION LIFELINE</a> 1-800-273-TALK</h2>
+		<h2><a href="http://www.suicidepreventionlifeline.org/">National suicide prevention lifeline</a> 1-800-273-TALK</h2>
 		<h3>Articles</h3>
 			<p><a href="http://www.huffingtonpost.co.uk/paul-bailey/building-confidence-top-tips_b_3032604.html">Twenty-Three Top Tips to Building Confidence</a></p>
 			<p><a href="http://www.webmd.com/anxiety-panic/guide/coping-with-anxiety">Coping with Anxiety</a></p>
@@ -20,5 +21,6 @@
 	</div>
 	<?php include 'leftsidebar.php'; ?>
 	<?php include 'rightsidebar.php'; ?>
+</div>
 <?php include 'footer.php'; ?>
 
