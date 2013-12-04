d1071a03f9f4e96075fe70984ce17bc027ab43eb
TAHA MEHDI <mehdit@rpi.edu>
2013-12-04 16:20:24 -0500

    journal notification

--- journal.php
+++ journal.php
@@ -1,11 +1,70 @@
 <?php include 'header.php';?>
+	<?php
 
+		if(!empty($_POST)){
+
+			$error_msg="";
+
+			if(empty($_POST['subject']))
+			{
+				$error_msg="Please include a subject line";
+				echo json_encode(array("error"=>$error_msg, "success"=>0));
+				die();
+			}
+
+			if(empty($_POST['entry'])){
+				$error_msg="Glad you're eager to post, but you forgot the entry!";
+				echo json_encode(array("error"=>$error_msg, "success"=>0));
+				die();
+			}
+
+			$query = "
+		            INSERT INTO journals (
+		            	uid,
+		            	author,
+		            	date,
+		                subject,
+		                content
+		            ) VALUES (
+		            	:uid,
+		            	:un,
+		            	:date,
+		                :subject,
+		                :entry
+	        	   )";
+			
+			$query_params = array(
+				':uid'     => $_POST['id'],
+				':un'      => $_POST['uname'],
+				'date'     => date("Y/m/d"),
+				':subject' => $_POST['subject'],
+				':entry'   => $_POST['entry']
+			);
+
+			try
+			{
+				$stmt = $db->prepare($query);
+				$result = $stmt->execute($query_params);
+				$notification = "Yay your entry has been recorded!!";
+			}catch(PDOException $ex)
+			{
+				$error_msg="Failed to run query: " . $ex->getMessage();
+				echo json_encode(array("error"=>$error_msg, "success"=>0));
+				die();
+			
+			}
+		}
+	?>
 <div class = "wrapmeup">
          <div class = "bodyBlock">
+		 	<?php 
+			if ((isset($notification)) && ($notification != NULL) &&($notification != ""))
+			echo "<div title='Notification!' id='notification'>".$notification."</div>"; ?>
+
                 <h1>JOURNAL</h1>
                 
 
-                        <form action="addJEntry.php" method="post" id="journal">
+                        <form action="" method="post" id="journal">
                                 <input type='hidden' name='id' id='id' value='<?php echo $_SESSION["user"]["id"]; ?>'>
                                 <input type='hidden' name='uname' id='uname' value='<?php echo $_SESSION["user"]["username"]; ?>'>
                                 <span>Subject: <input type="text" name="subject" style="width:500px;"/></span><br>
