c8fbaa9fb48797472e42e8e44fcc3bc47cd9bfe8
Hydriad <Hydriad@users.noreply.github.com>
2013-12-04 23:34:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -53,13 +53,13 @@
 						</div>
 					</div>
 					<div class="panel" title="Panel 6">
-						<div class="wrapper">
+						<div class="wrapperquote">
 							<h1>Quote of the Week</h1>
 							
 							<p>“I hope that in this year to come, you make mistakes.Because if you are making mistakes, then you are making new things, trying new things, learning, living, pushing yourself, changing yourself, changing your world. You're doing things you've never done before, and more importantly, you're Doing Something.
 										So that's my wish for you, and all of us, and my wish for myself. Make New Mistakes. Make glorious, amazing mistakes. Make mistakes nobody's ever made before. Don't freeze, don't stop, don't worry that it isn't good enough, or it isn't perfect, whatever it is: art, or love, or work or family or life. Whatever it is you're scared of doing, Do it.
 										Make your mistakes, next year and forever.”
-							</p><br/>
+							</p>
 							<p><em>- Neil Gaiman</em></p>
 						</div>
 					</div>
